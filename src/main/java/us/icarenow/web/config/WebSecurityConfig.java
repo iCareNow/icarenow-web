6256228ce7999f346e7e77bfe3c9de08087ba8ad
unknown <libenciuc.adrian@yahoo.com>
2022-06-06 23:11:02 +0300

    Created WebSecurityConfig

---
+++ src/main/java/us/icarenow/web/config/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+package us.icarenow.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private UserService userService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/img/**", "/js/**", "/vendor/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers( "/login", "/signup").anonymous()
+                .antMatchers("/doctor/**").hasRole("DOCTOR")
+                .antMatchers("/patient/**").hasRole("PATIENT")
+                .antMatchers("/admin/**","/h2-console/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("libenciuc.adrian@yahoo.com")
+                .password(passwordEncoder().encode("Password"))
+                .roles("ADMIN")
+                .and()
+                .withUser("libenciuc.adrian@yahoo.com")
+                .password(passwordEncoder().encode("Password"))
+                .roles("DOCTOR")
+                .and()
+                .withUser("libenciuc.adrian@yahoo.com")
+                .password(passwordEncoder().encode("Password"))
+                .roles("PATIENT");
+//        auth.userDetailsService(userService)
+//                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
