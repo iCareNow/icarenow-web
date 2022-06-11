9a1d3a186334c83f82cf94576aee6b1c1ad7f590
radubaciuDell <radumbaciu@gmail.com>
2022-06-12 02:30:00 +0300

    test

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into patient (id, CNP, nume, insured) values (1,'1890511123456','Baciu Radu',fal
+insert into patient (id, CNP, nume, insured) values (2,'1880111123456','Andrei Mladin',true);
