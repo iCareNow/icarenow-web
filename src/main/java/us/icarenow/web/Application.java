0f787ff882680476aa5c5e836fceca0ac2d56307
andrei <andrei.mladin@gmail.com>
2022-06-28 00:11:14 +0300

    CLEAN

--- src/main/java/us/icarenow/web/Application.java
+++ src/main/java/us/icarenow/web/Application.java
@@ -7,7 +7,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-//        System.out.println(new BCryptPasswordEncoder().encode("pascal"));
         SpringApplication.run(Application.class,args);
     }
 }
